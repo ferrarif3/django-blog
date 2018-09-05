7ebd0e0ab5fe81aefbdfcb86a75c83ce18c0b919
FrancoF <franco.ferrari3@gmail.com>
2018-09-05 01:41:49 -0300

    HTML changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('post/<int:pk>/', views.postdetail, name='postdetail'),
-    path('post/delete/<int:pk>/', views.deletepost, name='deletepost'),
-    path('post/new/', views.newpost, name='post_new'),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
