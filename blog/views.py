ddee7dfce46b391896df24a7ccffa23e32771714
FrancoF <franco.ferrari3@gmail.com>
2018-09-05 02:16:59 -0300

    Added templates

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'postlist.html', {})
+    posts = Post.objects.all()
+    return render(request, 'postlist.html', {'posts': posts})
