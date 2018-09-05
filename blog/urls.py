8e954a3a3cfe209f5f33c60b16cdb41599fe20f2
FrancoF <franco.ferrari3@gmail.com>
2018-09-05 13:28:50 -0300

    Added new post feature

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('post/<int:pk>/', views.postdetail, name='postdetail')
+    path('post/<int:pk>/', views.postdetail, name='postdetail'),
+    path('post/new/', views.newpost, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -13,3 +14,17 @@ def home(request):
 def postdetail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'postdetail.html', {'post': post})
+
+
+def newpost(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('postdetail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'newpost.html', {'form': form})
